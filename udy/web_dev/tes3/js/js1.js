012f00f6d1662077de3967172762da53579c1cc9
FANSAN6190 <fanin.s.pbl@gmail.com>
2022-09-18 09:46:18 +0530

    udy_js

--- udy/web_dev/tes3/js/js1.js
+++ udy/web_dev/tes3/js/js1.js
@@ -1,4 +1,4 @@
 function lifeInWeeks(age){
     var yl=90-age;
-    alert("You have "+(yl*365)+" days left, "+(yl*52)+" Weeks, and "+(yl*12)+" Months left");
+    console.log("You have "+(yl*365)+" days left, "+(yl*52)+" Weeks, and "+(yl*12)+" Months left");
 }
