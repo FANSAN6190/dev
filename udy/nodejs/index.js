d640c19999245176f3f6e16b56627d728da9a00e
FANSAN6190 <fanin.s.pbl@gmail.com>
2022-09-27 23:10:24 +0530

    node

---
+++ udy/nodejs/index.js
@@ -0,0 +1,6 @@
+var superheroes=require("superheroes");
+var mSN=superheroes.random()
+console.log(mSN);
+var supervillains=require("supervillains");
+var mSV=supervillains.random()
+console.log(mSV);
