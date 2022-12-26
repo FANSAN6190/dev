5b24730ad76651d394f83f3e6909f152f5cd1419
FANSAN6190 <fanin.s.pbl@gmail.com>
2022-12-26 21:29:24 +0530

    ejs-2

--- udy/todolist-v1/app.js
+++ udy/todolist-v1/app.js
@@ -7,11 +7,37 @@ app.get("/",function(req,res){
     var today=new Date();
     var currentday=today.getDay();
     var day="";
-    if(currentday===6 || currentday===0){
+    /*if(currentday===6 || currentday===0){
         day="Weekend";
     }
     else{
         day="Weekday";
+    }*/
+    switch (currentday) {
+        case 0:
+            day="Sunday"
+            break;
+        case 1:
+            day="Monday"
+            break;
+        case 2:
+            day="Tuesday"
+            break;
+        case 3:
+            day="Wednesday"
+            break;
+        case 4:
+            day="Thursday"
+            break;
+        case 5:
+            day="Friday"
+            break;
+        case 6:
+            day="Saturday"
+            break;
+        default:
+            day="\"an ERROR\""
+            break;
     }
     res.render("list.ejs",{day_today:day});
 });
