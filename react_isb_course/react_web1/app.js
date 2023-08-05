b3c83d68631a9a1f93a1ca043d5c8cdf452f6ca0
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-08-05 22:01:37 +0530

    react

---
+++ react_isb_course/react_web1/app.js
@@ -0,0 +1,5 @@
+//var h1=React.createElement('h1',null,'Hello World.');
+//ReactDOM.render(h1,document.getElementById('app'));
+
+
+ReactDOM.render(<h1>Hello World </h1>,document.getElementById('app'));
